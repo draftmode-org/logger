bb5fbd7136dcf8ce5e6b2a4f5a8c0cb62908b3ab
hashTag <48600109+terrazza@users.noreply.github.com>
2021-12-22 23:13:17 +0100

    start implementing exception handling rcf standard for psr/log

--- src/Formatter/FormatterTrait.php
+++ src/Formatter/FormatterTrait.php
@@ -1,20 +1,117 @@
 <?php
 namespace Terrazza\Component\Logger\Formatter;
 
-trait FormatterTrait {
+use Throwable;
+
+trait FormatterTrait
+{
     /**
      * @param array $token
      * @param string $findKey
-     * @return array|mixed|null
+     * @return mixed|null
      */
-    private function getTokenValue(array $token, string $findKey) {
-        foreach (explode(".", $findKey) as $tokenKey) {
-            if (array_key_exists($tokenKey, $token)) {
-                $token								= $token[$tokenKey];
+    private function getTokenValue(array $token, string $findKey)
+    {
+        $tokenKeys = explode(".", $findKey);
+        while (count($tokenKeys)) {
+            $tokenKey = array_shift($tokenKeys);
+            if (is_array($token)) {
+                if (array_key_exists($tokenKey, $token)) {
+                    $token = $token[$tokenKey];
+                } elseif ($tokenKey === "*") {
+                    return $token;
+                } else {
+                    return null;
+                }
             } else {
                 return null;
+            }
+            if ($token instanceof Throwable) {
+                $traceMessageMax                    = array_shift($tokenKeys);
+                $traceMessageArgs                   = array_shift($tokenKeys);
+                $this->traceMessageMax              = $traceMessageMax ?? 0;
+                $this->traceMessageArgs             = (bool)$traceMessageArgs;
+                $this->convertException($token);
+                return $this->traceMessage;
             }
         }
         return $token;
     }
+
+    private int $traceMessageMax                    = 0;
+    private bool $traceMessageArgs                  = false;
+    private array $traceMessage                     = [];
+
+    /**
+     * @param array $traceMessage
+     * @return bool
+     */
+    private function pushMessage(array $traceMessage) : bool {
+        if (!$this->traceMessageMax || $this->traceMessageMax > count($this->traceMessage)) {
+            array_push($this->traceMessage, $traceMessage);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param array $trace
+     * @param $iTrace
+     * @return array|string[]
+     */
+    private function getTraceMessage(array $trace, $iTrace) : array {
+        $line                                       = "-";
+        $file                                       = "-";
+        if (array_key_exists($iTrace, $trace)) {
+            $line                                   = $trace[$iTrace]["line"] ?? "-";
+            $file                                   = $trace[$iTrace]["file"] ?? "-";
+        }
+        $function                                   = "-";
+        $class                                      = "-";
+        if (array_key_exists($iTrace+1, $trace)) {
+            $function                               = $trace[$iTrace+1]["function"] ?? "-";
+            $class                                  = $trace[$iTrace+1]["class"] ?? "-";
+        }
+        $message = [
+            "line"                                  => $line,
+            "function"                              => $function,
+            "class"                                 => $class,
+            "file"                                  => $file,
+        ];
+        if ($this->traceMessageArgs && array_key_exists("args", $trace[$iTrace])) {
+            $message["args"]                        = $trace[$iTrace]["args"];
+        }
+        return $message;
+    }
+
+    /**
+     * @param Throwable $exception
+     * @param Throwable|null $parent
+     * @return void
+     */
+    private function convertException(Throwable $exception, Throwable $parent=null) : void {
+        $trace                                      = $exception->getTrace();
+        for ($iTrace = 0; $iTrace < count($trace); $iTrace=$iTrace+2) {
+            $message                                = $this->getTraceMessage($trace, $iTrace);
+            if (!$this->pushMessage($message)) break;
+            if ($previous = $exception->getPrevious()) {
+                $this->convertException($previous, $exception);
+                $pTrace                             = $previous->getTrace();
+                $message = [
+                    "message"                       => $previous->getMessage(),
+                    "line"                          => $previous->getLine(),
+                    "function"                      => $pTrace[0]["function"] ?? "-",
+                    "class"                         => $pTrace[0]["class"] ?? "-",
+                ];
+                if ($this->traceMessageArgs && array_key_exists("args", $pTrace[0])) {
+                    $message["args"]                = $pTrace[0]["args"];
+                }
+                if (!$this->pushMessage($message)) break;
+                return;
+            }
+            if ($parent) return;
+        }
+    }
+
 }
--- tests/Formatter/FormatterTraitTest.php
+++ tests/Formatter/FormatterTraitTest.php
@@ -2,6 +2,7 @@
 namespace Terrazza\Component\Logger\Tests\Formatter;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\TextUI\RuntimeException;
 use Terrazza\Component\Logger\Formatter\FormatterTrait;
 
 class FormatterTraitTest extends TestCase {
@@ -54,8 +55,163 @@ class FormatterTraitTest extends TestCase {
         $token      = [$tKey = "tKey" => [$tInKey = "tInKey" => [$tInInKey = "tInInKey" => $tValue = ["tInValue"]]]];
         $this->assertEquals($tValue, $formatter->_getTokenValue($token, "$tKey.$tInKey.$tInInKey"));
     }
-}
 
+    function testInArrayAll() {
+        $formatter  = new FormatterTraitTestTrait();
+        $token      = ["Context" => $context = ["key1" => "value1", "key2" => "value2"], "someOtherKey" => "someOtherValue"];
+        $actual     = $formatter->_getTokenValue($token, "Context.*");
+        $this->assertEquals($context, $actual);
+    }
+
+    function xtestException() {
+        $formatter  = new FormatterTraitTestTrait();
+        $token      = ["exception" => new RuntimeException("myException")];
+        $trace      = $formatter->_getTokenValue($token, "exception");
+        $this->assertCount(10, $trace);
+    }
+
+    public int $exceptionLine=0;
+    private function getException(int $arg) {
+        $this->exceptionLine = __LINE__ + 1;
+        throw new RuntimeException("exception in ".__METHOD__);
+    }
+
+    public int $exception1ParentLine=0;
+    private function getExceptionWith1Parent(int $arg) {
+        try {
+            $this->exception1ParentLine = __LINE__ + 1;
+            $this->getException($arg*2);
+        }
+        catch (\Throwable $exception) {
+            throw new RuntimeException("exception in ".__METHOD__, 0, $exception);
+        }
+    }
+
+    public int $exception2ParentLine=0;
+    private function getExceptionWith2Parents(int $arg) {
+        try {
+            $this->exception2ParentLine = __LINE__ + 1;
+            $this->getExceptionWith1Parent($arg*2);
+        }
+        catch (\Throwable $exception) {
+            throw new RuntimeException("exception in ".__METHOD__, 0, $exception);
+        }
+    }
+
+    function xtestExceptionLevel3() {
+        $formatter      = new FormatterTraitTestTrait();
+        $token          = [];
+        try {
+            $this->getException(12);
+        } catch (\Throwable $exception) {
+            $token      = ["exception" => $exception];
+        }
+        $traceCount = 3;
+        $trace      = $formatter->_getTokenValue($token, "exception.".$traceCount.".2");
+        $this->assertCount($traceCount, $trace);
+    }
+
+    function testExceptionWith1Parent() {
+        $formatter      = new FormatterTraitTestTrait();
+        $token          = [];
+        $parent1Class   = new FormatterTraitTestExceptionParent1;
+        try {
+            $line       = __LINE__ + 1;
+            $parent1Class->getExceptionWith1Parent(12);
+        } catch (\Throwable $exception) {
+            $token      = ["exception" => $exception];
+        }
+        $traceCount = 3;
+        $trace      = $formatter->_getTokenValue($token, "exception.".$traceCount.".2");
+        $this->assertEquals([
+            $traceCount,
+            $line,
+            $parent1Class->getExceptionLine(),
+            (new FormatterTraitTestExceptionMain)->getExceptionLine(),
+        ],[
+            count($trace),
+            $trace[0]["line"],
+            $trace[1]["line"],
+            $trace[2]["line"]
+        ]);
+    }
+
+    function testExceptionWith2Parents() {
+        $formatter      = new FormatterTraitTestTrait();
+        $token          = [];
+        $parent2Class   = new FormatterTraitTestExceptionParent2;
+        try {
+            $line       = __LINE__ + 1;
+            $parent2Class->getExceptionWith2Parent($arg = 12);
+        } catch (\Throwable $exception) {
+            $token      = ["exception" => $exception];
+        }
+        $traceCount     = 4;
+        $trace          = $formatter->_getTokenValue($token, "exception.".$traceCount.".1");
+        $this->assertEquals([
+            $traceCount,
+            $line,
+            $parent2Class->getExceptionLine(),
+            (new FormatterTraitTestExceptionParent1)->getExceptionLine(),
+            (new FormatterTraitTestExceptionMain)->getExceptionLine(),
+
+            [$arg],
+            [$arg*2],
+            [$arg*4],
+            [$arg*4],
+        ],[
+            count($trace),
+            $trace[0]["line"],
+            $trace[1]["line"],
+            $trace[2]["line"],
+            $trace[3]["line"],
+
+            $trace[0]["args"],
+            $trace[1]["args"],
+            $trace[2]["args"],
+            $trace[3]["args"],
+        ]);
+    }
+}
+class FormatterTraitTestExceptionMain {
+    public int $exceptionLine=__LINE__+2;
+    public function getException(int $arg) {
+        throw new RuntimeException("exception in ".__METHOD__);
+    }
+    public function getExceptionLine() :int {
+        return $this->exceptionLine;
+    }
+}
+class FormatterTraitTestExceptionParent2 {
+    public int $exceptionLine=__LINE__+4;
+    public function getExceptionWith2Parent(int $arg) {
+        try {
+            $parent1Exception = new FormatterTraitTestExceptionParent1();
+            $parent1Exception->getExceptionWith1Parent($arg*2);
+        }
+        catch (\Throwable $exception) {
+            throw new RuntimeException("exception in ".__METHOD__, 0, $exception);
+        }
+    }
+    public function getExceptionLine() :int {
+        return $this->exceptionLine;
+    }
+}
+class FormatterTraitTestExceptionParent1 {
+    public int $exceptionLine=__LINE__+4;
+    public function getExceptionWith1Parent(int $arg) {
+        try {
+            $mainException = new FormatterTraitTestExceptionMain();
+            $mainException->getException($arg*2);
+        }
+        catch (\Throwable $exception) {
+            throw new RuntimeException("exception in ".__METHOD__, 0, $exception);
+        }
+    }
+    public function getExceptionLine() :int {
+        return $this->exceptionLine;
+    }
+}
 class FormatterTraitTestTrait {
     use FormatterTrait;
     public function _getTokenValue(array $token, string $findKey) {
